3cf791d69831b61efed36b8937b7d38ab0c675b4
SONIYA PANDA_MCA_1A_52_UEMK <pandasoniya121@gmail.com>
2024-08-23 19:55:30 +0530

    Create 1.C

---
+++ Assignment 1/1.C
@@ -0,0 +1,15 @@
+//Print sum and average of three numbers
+#include<stdio.h>
+float num1,num2,num3;
+printf("Enter the first number");
+scanf("%d",&num1);
+printf("enter the second number");
+scanf("%d",&num2);
+printf("enter the third number");
+scanf("%d",&num3);
+sum=num1+num2+num3;
+average=sum/3;
+printf("sum of the numbers:%.2f\n",sum);
+printf("Average of the numbers:%2f\n",average);
+return 0;
+}
