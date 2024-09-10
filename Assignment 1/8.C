e7c647263751277e69de461675a337f0fefddab0
SONIYA PANDA_MCA_1A_52_UEMK <pandasoniya121@gmail.com>
2024-09-10 21:22:48 +0530

    Update 8.C

--- Assignment 1/8.C
+++ Assignment 1/8.C
@@ -1,26 +1,22 @@
-//A number is perfect or not
+//A number is strong or not
 #include<stdio.h>
-int main()
-{
+int main(){
 int num,sum=0,i;
 printf("Enter a positive number");
-scanf("%d",&num);
-if(num<0)
-{
-printf(The num must be a positive integer:);
+scanf("%d",&number);
+if(num<=0){
+printf("The num must be a positive integer\n");
 return 1;
 }
 for(i=1;i<=num/2;i++)
-{
 if(num%i==0){
 sum+=i;
 }
 }
 if(sum==num){
-printf("%d is aperfect num \n",num);
-}
-else{
-printf("%d is not a perfect num \n",num);
+printf(%d is a perfect num.\n",num);
+}else{
+printf("%d is not a perfect num.\n",num);
 }
 return 0;
 }
