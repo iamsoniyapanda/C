601eb74426a45c49b5dec6356b3dbb3e24488f2e
SONIYA PANDA_MCA_1A_52_UEMK <pandasoniya121@gmail.com>
2024-09-11 22:59:46 +0530

    Create 16.c

---
+++ Assignment 1/16.c
@@ -0,0 +1,15 @@
+// A number is even or odd
+#include<stdio.h>
+int main()
+{
+  int num;
+printf("Enter an integer");
+scanf("%d",&num);
+
+if(num%2==0){
+printf("%d is  an even number.\n");
+}else{
+printf("%d is an odd number .\n");
+}
+return0;
+}
