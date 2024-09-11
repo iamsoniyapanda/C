c90111d4eb82c1e8eebfeac052cb66998811834b
SONIYA PANDA_MCA_1A_52_UEMK <pandasoniya121@gmail.com>
2024-09-11 22:19:57 +0530

    Create 14.c

---
+++ Assignment 1/14.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int num;
+printf("Enter an integer:");
+scanf("%d",&num);
+
+if(number>0)
+{
+printf("The number %d is positive \n ,num");
+}else if(num<0)
+{
+printf("The number %d is negative \n",num);
+}
+else {
+printf("The num is zero\n");
+}
+return 0;
+}
