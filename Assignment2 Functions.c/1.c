89aed38da107c31a313d1e6c18aecefdc8311ba8
SONIYA PANDA_MCA_1A_52_UEMK <pandasoniya121@gmail.com>
2024-09-13 20:16:45 +0530

    Create 1.c

---
+++ Assignment2 Functions.c/1.c
@@ -0,0 +1,17 @@
+//Add ,substract,multiply & divide two integers using a user-defined type function with a return type
+#include<stdio.h>
+int main()
+{
+  int num1,num2,res;
+printf("entter the two num:");
+scanf("%d%d",&num1,&num2);
+res=num1+num2;
+printf("\n Addition=%d",res);
+res=num1-num2;
+ptintf("\n Substraction =%d,res");
+res=num1*num2;
+printf("\n Multiplication=%d",res");
+res=num1/num2;
+printf("\n Divison =%d\n",res);
+return 0;
+}
