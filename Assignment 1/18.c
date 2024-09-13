bd74c53c16f952f6c80f7d9bd06ef7d3a907bb08
SONIYA PANDA_MCA_1A_52_UEMK <pandasoniya121@gmail.com>
2024-09-13 19:42:33 +0530

    Create 18.c

---
+++ Assignment 1/18.c
@@ -0,0 +1,13 @@
+//A character is alphabet or not
+#include<stdio.h>
+int main(){
+char ch;
+printf("Enter a character:");
+scanf("%c",&ch);
+if(isalpha(ch)){
+printf("%c is an alphabet\n",ch);
+}else{
+printf("%c is not an alphabet\n",ch);
+}
+return 0;
+}
