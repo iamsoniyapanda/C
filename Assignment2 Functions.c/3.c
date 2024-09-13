a13c88c58c9fb82563a918a9419302b7792de322
SONIYA PANDA_MCA_1A_52_UEMK <pandasoniya121@gmail.com>
2024-09-13 20:58:02 +0530

    Create 3.c

---
+++ Assignment2 Functions.c/3.c
@@ -0,0 +1,18 @@
+//A fibonacci series using recursive function
+#include<stdio.h.>
+int fibonacchi(int n){
+  if(n<==1)
+    return ;
+return fibonacci(n-1)+fibonacchi(n-2);
+}
+int main(){
+  int n,i;
+printf("Enter the number of terms:");
+scanf("%d"&n);
+printf("Fibonacci series:");
+for()i=0;i<n;i++)
+{
+fibonacci(i);
+}
+return 0;
+}
