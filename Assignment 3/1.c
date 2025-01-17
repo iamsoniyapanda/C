2dd1b161f57e6aedf0a0e1ccdd0099e547b41b2d
SONIYA PANDA_MCA_1A_47_UEMK_ 2024-2026 <pandasoniya121@gmail.com>
2025-01-17 22:53:56 +0530

    Create 1.c

---
+++ Assignment 3/1.c
@@ -0,0 +1,19 @@
+//Write a program to store marks for n number of student in an array and print their marks. 
+#include <stdio.h> 
+int main() { 
+    printf("ROHAN DAS-MCA-A-1-Roll-42\n"); 
+    int n, i; 
+    printf("Enter the number of students: "); 
+    scanf("%d", &n); 
+    int marks[n]; 
+    printf("Enter the marks of %d students:\n", n); 
+    for (i = 0; i < n; i++) { 
+        printf("Marks for student %d: ", i + 1); 
+        scanf("%d", &marks[i]); 
+    } 
+    printf("\nThe marks of the students are:\n"); 
+    for (i = 0; i < n; i++) { 
+        printf("Student %d: %d\n", i + 1, marks[i]); 
+     } 
+return 0; 
+}
