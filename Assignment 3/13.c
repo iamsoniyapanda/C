d885bc0cabecff0a89ec3cfb51d518c18d73b83c
SONIYA PANDA_MCA_1A_47_UEMK_ 2024-2026 <pandasoniya121@gmail.com>
2025-01-17 23:12:03 +0530

    Create 13.c

---
+++ Assignment 3/13.c
@@ -0,0 +1,28 @@
+// Write a program to transpose a matrix. 
+#include <stdio.h> 
+int main() {  
+    int r, c, i, j; 
+    printf("Enter the number of rows and columns: "); 
+    scanf("%d %d", &r, &c); 
+    int mat[r][c], transpose[c][r]; 
+    printf("Enter the elements of the matrix:\n"); 
+    for (i = 0; i < r; i++) { 
+        for (j = 0; j < c; j++) { 
+            scanf("%d", &mat[i][j]); 
+        } 
+    } 
+    for (i = 0; i < r; i++) { 
+        for (j = 0; j < c; j++) { 
+            transpose[j][i] = mat[i][j]; 
+        } 
+    } 
+    printf("Transpose of the matrix:\n"); 
+    for (i = 0; i < c; i++) { 
+        for (j = 0; j < r; j++) { 
+            printf("%d ", transpose[i][j]); 
+        } 
+        printf("\n"); 
+    } 
+    return 0; 
+} 
+Output: 
