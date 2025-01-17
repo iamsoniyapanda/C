48aa8168818a175ac152ba881002a30fd9cf7389
SONIYA PANDA_MCA_1A_47_UEMK_ 2024-2026 <pandasoniya121@gmail.com>
2025-01-17 23:25:06 +0530

    Create 1.c

---
+++ Assignment 4 File/1.c
@@ -0,0 +1,15 @@
+//Write a C program to list all files and sub-directories in a directory.
+ #include <stdio.h>
+ #include <dirent.h>
+ int main() {
+ struct dirent *de;
+ DIR *dr = opendir(".");
+ if (dr == NULL) {
+ printf("Could not open current directory");
+ return 0;
+ }
+ while ((de = readdir(dr)) != NULL)
+ printf("%s\n", de->d_name);
+ closedir(dr);
+ return 0;
+ }
